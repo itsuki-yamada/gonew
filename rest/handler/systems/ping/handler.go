3edbe94b7b5bc74b6ec8e0a4cb2eb27f85637cc3
ItsukiYamada <itsuki.yamada@uzabase.com>
2024-03-13 23:21:58 +0900

    :recycle:refactor:

--- rest/handler/systems/ping/handler.go
+++ rest/handler/systems/ping/handler.go
@@ -2,33 +2,22 @@ package rest
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/itsuki-yamada/gonew/domain"
-	"github.com/labstack/echo/v4"
 )
 
 type (
 	PingHandler struct {
-		usecase PingUsecase
+		usecase FetchPingUsecase
 	}
 
-	PingUsecase interface {
-		FetchPing(c context.Context, ping domain.Pinger) domain.Message
+	FetchPingUsecase interface {
+		Exec(c context.Context, ping domain.Pinger) domain.Message
 	}
 )
 
-func NewPingHandler(u PingUsecase) *PingHandler {
+func NewPingHandler(u FetchPingUsecase) *PingHandler {
 	return &PingHandler{
 		usecase: u,
 	}
-}
-
-func (h *PingHandler) Get(c echo.Context) error {
-	msg := c.QueryParam("msg")
-	ping := domain.NewPinger(msg)
-
-	ctx := c.Request().Context()
-	resText := h.usecase.FetchPing(ctx, ping)
-	return c.String(http.StatusOK, string(resText))
 }
--- rest/server.go
+++ rest/server.go
@@ -2,18 +2,16 @@ package rest
 
 import (
 	rest "github.com/itsuki-yamada/gonew/rest/handler/systems/ping"
-	usecase "github.com/itsuki-yamada/gonew/usecase/systems/ping"
+	"github.com/itsuki-yamada/gonew/usecase"
 	"github.com/labstack/echo/v4"
 )
 
-func initRouting(e *echo.Echo) {
-	pingHandler := rest.NewPingHandler(&usecase.PingUsecase{})
-	g := e.Group("/systems")
-	g.GET("/ping", pingHandler.Get)
-}
-
 func Run() {
 	e := echo.New()
-	initRouting(e)
+
+	pingHandler := rest.NewPingHandler(&usecase.FetchPingUsecase{})
+	g := e.Group("/systems")
+	g.GET("/ping", pingHandler.Get)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
